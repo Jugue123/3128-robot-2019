1070861f499be6b0e12410e85b14992599f0fb34
jude-lif <43428384+Jugue123@users.noreply.github.com>
2019-08-31 19:55:12 -0700

    Merge remote-tracking branch 'upstream/master'

--- src/main/java/org/team3128/gromit/main/MainDeepSpaceRobot.java
+++ src/main/java/org/team3128/gromit/main/MainDeepSpaceRobot.java
@@ -278,6 +278,7 @@ public class MainDeepSpaceRobot extends NarwhalRobot{
 
 		rightJoystick = new Joystick(0);
 		listenerRight = new ListenerManager(rightJoystick);
+		//listenerLeft = new ListenerManager(leftJoystick);
 		addListenerManager(listenerRight);
 
 		topLimelight =    new Limelight("limelight-top",    topLLAngle,     topLLHeight,    23 * Length.in, 14.5 * Length.in);
@@ -388,6 +389,35 @@ public class MainDeepSpaceRobot extends NarwhalRobot{
 			ledOn = !ledOn;
 		});
 
+<<<<<<< HEAD
+=======
+		NarwhalDashboard.addButton("lift_up", (boolean down) -> {
+			if (down) {
+				lift.powerControl(0.8);
+			}
+			else {
+				lift.powerControl(0);
+			}
+		});
+
+		NarwhalDashboard.addButton("lift_down", (boolean down) -> {
+			if (down) {
+				lift.powerControl(-0.2);
+			}
+			else {
+				lift.powerControl(0);
+			}
+		});
+
+		NarwhalDashboard.addButton("compute2D", (boolean down) -> {
+			if (down) {
+				Compute2DLocalization locale = Compute2D.compute2D(bottomLimelight, Compute2D.getInput(bottomLimelight, 3), DeepSpaceConstants.LOW_VISION_TARGET_HEIGHT);
+
+				Log.info("MainDeepSpaceRobot", "locale = " + locale);
+			}
+		});
+
+>>>>>>> upstream/master
 		dcu.initNarwhalDashboard();
     }
 
@@ -411,8 +441,8 @@ public class MainDeepSpaceRobot extends NarwhalRobot{
 			}
 		}, "MoveForwards", "MoveTurn", "Throttle");
 
-		listenerRight.nameControl(new Button(2), "Gearshift");
-		listenerRight.addButtonDownListener("Gearshift", drive::shift);
+		//listenerRight.nameControl(new Button(2), "Gearshift");
+		//listenerRight.addButtonDownListener("Gearshift", drive::shift);
 
 		// Intake/Outtake Controls
 		listenerRight.nameControl(new Button(5), "DemogorgonGrab");
@@ -521,6 +551,7 @@ public class MainDeepSpaceRobot extends NarwhalRobot{
 		listenerLeft.nameControl(ControllerExtreme3D.JOYY, "ManualControl");
 
 		listenerLeft.addMultiListener(() -> {
+			
 			if (listenerLeft.getButton("ManualMode")) {
 				lift.override = false;
 				lift.powerControl(0);
@@ -645,7 +676,7 @@ public class MainDeepSpaceRobot extends NarwhalRobot{
 		//Log.info("roll, pitch", String.valueOf(gyro.getRoll()) + ", " + String.valueOf(gyro.getPitch()));
 
 		maxLeftSpeed = Math.max(leftDriveLeader.getSelectedSensorVelocity(), maxLeftSpeed);
-        maxRightSpeed = Math.max(rightDriveLeader.getSelectedSensorVelocity(), maxRightSpeed);
+		maxRightSpeed = Math.max(rightDriveLeader.getSelectedSensorVelocity(), maxRightSpeed);
 
         SmartDashboard.putNumber("Max Left Speed", maxLeftSpeed);
         SmartDashboard.putNumber("Max Right Speed", maxRightSpeed);
@@ -653,6 +684,7 @@ public class MainDeepSpaceRobot extends NarwhalRobot{
 		SmartDashboard.putNumber("pitch", gyro.getPitch());
 		SmartDashboard.putBoolean("Lift: Can Raise", lift.canRaise);
 		SmartDashboard.putBoolean("Lift: Can Lower", lift.canLower);
+		SmartDashboard.putBoolean("Lift Switch Triggered", lift.getLimitSwitch());
 
 		SmartDashboard.putNumber("Lift Height (inches)", lift.getCurrentHeight() / Length.in);
 		SmartDashboard.putNumber("Lift Position (nu)", liftMotorLeader.getSelectedSensorPosition());
